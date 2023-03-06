9879018f62e0503ffd70264cda03abf74e041c48
Bhupendra Parihar <bhupendraparihar@outlook.com>
2023-03-06 20:35:40 +0530

    stop method added in engine classes

--- DiselEngine.java
+++ DiselEngine.java
@@ -2,4 +2,8 @@ public class DiselEnigne{
 	public void start() {
 		System.out.println("Disel engine started");
 	}
+	
+	public void stop() {
+		System.out.println("Disel engine stoped");
+	}
 }
--- PetrolEngine.java
+++ PetrolEngine.java
@@ -2,4 +2,8 @@ public class PetrolEngine {
 	public void start() {
 		System.out.println("Petrol Engine Started");
 	}
+	
+	public void stop() {
+		System.out.println("Disel engine stoped");
+	}
 }
