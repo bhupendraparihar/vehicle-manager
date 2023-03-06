8be58fa3a10c891812a5b0bd606025c6f017b268
Bhupendra Parihar <bhupendraparihar@outlook.com>
2023-03-06 20:19:16 +0530

    PetrolEngine class added

--- Vehicle.java
+++ Vehicle.java
@@ -1,9 +1,7 @@
 public class Vehicle {
 	
-	private PetrolEngine pE;
-	
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		PetrolEngine pE = new PetrolEngine();
 		pE.start();
 	}
 }
