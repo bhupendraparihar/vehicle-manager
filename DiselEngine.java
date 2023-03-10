d245129e1221452e02579b4dbb97f1f097d65da0
Bhupendra Parihar <bhupendraparihar@outlook.com>
2023-03-10 19:42:46 +0530

    Added power and fuel to disel engine

--- DiselEngine.java
+++ DiselEngine.java
@@ -1,9 +1,17 @@
-public class DiselEnigne{
+public class DieselEngine{
 	public void start() {
-		System.out.println("Disel engine started");
+		System.out.println("Diesel engine started");
 	}
 	
 	public void stop() {
-		System.out.println("Disel engine stoped");
+		System.out.println("Diesel engine stopped");
+	}
+
+	public void generatePower(){
+		System.out.println("Diesel engine generating power");
+	}
+
+	public void addingFuel(){
+		System.out.println("Diesel engine adding fuel");
 	}
 }
